5a5f19c39e1b4ed3ef0b85c87517fa9cd0735992
Samin.asnaashari <asn.samin@gmail.com>
2019-01-20 19:56:26 +0100

    initial

---
+++ battle-scars/build/precache-manifest.03ae08846f50f20d81db77cd43fc30be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ae34d8c8113bc8923896",
+    "url": "/static/css/main.23c31146.chunk.css"
+  },
+  {
+    "revision": "ae34d8c8113bc8923896",
+    "url": "/static/js/main.ae34d8c8.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "9fb4d7ecfb12560de2997c3b06606924",
+    "url": "/index.html"
+  }
+];
